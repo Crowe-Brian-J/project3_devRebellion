9f1e3d7dce3e8ec7a68b3abba37af49afa4a4f4e
Brian Crowe <brian.j.crowe@protonmail.com>
2023-06-21 14:32:22 -0400

    FeedForm update

--- devrebellion/main_app/forms.py
+++ devrebellion/main_app/forms.py
@@ -1,19 +1,27 @@
 from django.forms import ModelForm
-from .models import Comment, User, Developer
-from django import forms 
+from .models import Comment, User, Developer, Feed
+from django import forms
 
 
 class CommentForm(ModelForm):
     class Meta:
-        model=Comment 
-        fields=['text']
+        model = Comment
+        fields = ["text"]
+
 
 class UserForm(forms.ModelForm):
-    class Meta: 
+    class Meta:
         model = User
-        fields =('first_name', 'last_name', 'email')
+        fields = ("first_name", "last_name", "email")
+
 
 class DeveloperForm(forms.ModelForm):
-    class Meta: 
+    class Meta:
         model = Developer
-        fields = ('link',)
+        fields = ("link",)
+
+
+class FeedForm(forms.ModelForm):
+    class Meta:
+        model = Feed
+        fields = ("text",)
