231844b29601dabc21be83e8b76d774018b21ccb
Mayte Ozoria <mayteozoria@gmail.com>
2023-06-22 16:14:31 -0400

    Edit developer

--- devrebellion/main_app/views.py
+++ devrebellion/main_app/views.py
@@ -88,12 +88,39 @@ def developers_detail(request, developer_user_id):
 
 # @login_required
 def delete_developer(request, developer_user_id):
-    if request.user.id == developer_user_id:
-        developer = User.objects.get(id=developer_user_id)
-        developer.delete()
-        return redirect("about")
-    else: 
+  if request.user.id == developer_user_id:
+       developer = User.objects.get(id=developer_user_id)
+       developer.delete()
+       return redirect("about")
+  else: 
         return redirect("developers_index")
+
+# ----new edit ---
+# might have to remove developer_id argument 
+def edit_developer(request, developer_user_id):
+
+    if request.method == 'POST':
+        # d = User.objects.get(id=developer_user_id)
+        user_form = UserForm(request.POST, instance=request.user)
+        developer_form = DeveloperForm(request.POST, instance=request.user)
+        if user_form.is_valid() and developer_form.is_valid():
+            print('user')
+            user_form.save()
+            developer_form.save()
+            return redirect('developers_detail', developer_user_id)
+    else: 
+        d = User.objects.get(id=developer_user_id)
+        user_form = UserForm(request.POST, instance=request.user)
+        developer_form = DeveloperForm(request.POST, instance=request.user)
+    return render(
+        request,
+        "registration/profile.html",
+        {  # comeback to this line
+            "user_form": user_form,
+            "developer_form": developer_form,
+            
+        },
+    )
 
 def projects_index(request):
     projects = Project.objects.all()
